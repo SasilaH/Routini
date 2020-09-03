9bf8f979e59d9b12d551fe525fac2121eacb09a4
Sasila Hapuarachchi <sasilaah@gmail.com>
2020-09-02 20:36:36 -0400

    Added user signup and login endpoints

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -27,57 +27,94 @@ type Entry struct {
 }
 
 type User struct {
-	ID   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name string             `json:"name,omitempty" bson:"name,omitempty"`
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password string             `json:"password,omitempty" bson:"password,omitempty"`
 }
 
 var client *mongo.Client
 
-func GenerateUserEndpoint(response http.ResponseWriter, request *http.Request) {
+func UserSignupEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
+
 	var user User
+
 	if err := json.NewDecoder(request.Body).Decode(&user); err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
 		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
 		return
 	}
+
 	userCollection := client.Database("db_1").Collection("users")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	hash, _ := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	user.Password = string(hash)
+
 	result, err := userCollection.InsertOne(ctx, user)
+
 	if err != nil {
 		response.WriteHeader(http.StatusInternalServerError)
 		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
 		return
 	}
+
 	json.NewEncoder(response).Encode(result)
 }
 
+func UserLoginEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+
+	var inputUser User
+
+	if err := json.NewDecoder(request.Body).Decode(&inputUser); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+
+	var returnUser User
+
+	userCollection := client.Database("db_1").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	if err := userCollection.FindOne(ctx, bson.M{"email": inputUser.Email}).Decode(&returnUser); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(returnUser.Password), []byte(inputUser.Password)); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+
+	json.NewEncoder(response).Encode(returnUser)
+}
+
 func main() {
-	fmt.Println("111111")
 
-	client, _ = mongo.NewClient(options.Client().ApplyURI("mongodb+srv://userS:Teemoteemo123@cluster0.ge7wb.azure.mongodb.net/db?retryWrites=true&w=majority"))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	fmt.Println("222222")
+	var err error
+	client, err = mongo.NewClient(options.Client().ApplyURI("mongodb+srv://userS:Teemoteemo123@cluster0.ge7wb.azure.mongodb.net/db?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err := client.Connect(ctx)
+	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Disconnect(ctx)
 
-	fmt.Println("3333333")
-
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(databases)
+	// databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(databases)
 
 	router := mux.NewRouter()
-	router.HandleFunc("/user", GenerateUserEndpoint).Methods("POST")
+	router.HandleFunc("/user", UserSignupEndpoint).Methods("POST")
+	router.HandleFunc("/user", UserLoginEndpoint).Methods("GET")
 	http.ListenAndServe(":12345", router)
 }
